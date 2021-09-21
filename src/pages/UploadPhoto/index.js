b9782e8a319cf2b142be7b36d7786d34b18c8a2f
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-21 08:37:12 +0700

    Upload Photo Page

---
+++ src/pages/UploadPhoto/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {Header, Button, Link, Gap} from '../../components';
+import {IconAddPhoto, ILNullPhoto} from '../../assets';
+import {colors, fonts} from '../../utils';
+
+const UploadPhoto = () => {
+  return (
+    <View style={styles.page}>
+      <Header title="Upload Photo" />
+      <View style={styles.content}>
+        <View style={styles.profile}>
+          <View style={styles.avatarWrapper}>
+            <Image source={ILNullPhoto} style={styles.avatar} />
+            <IconAddPhoto style={styles.addPhoto} />
+          </View>
+          <Text style={styles.name}>Shayna Melinda</Text>
+          <Text style={styles.profession}>Product Designer</Text>
+        </View>
+        <View>
+          <Button title="Upload and Continue" />
+          <Gap height={30} />
+          <Link title="Skip for this" textAlign="center" fontSize={16} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default UploadPhoto;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  profile: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  content: {
+    paddingHorizontal: 40,
+    paddingBottom: 64,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  avatar: {
+    width: 110,
+    height: 110,
+    borderRadius: 110 / 2,
+  },
+  avatarWrapper: {
+    width: 130,
+    height: 130,
+    borderWidth: 1,
+    borderColor: colors.border,
+    borderRadius: 130 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  addPhoto: {
+    position: 'absolute',
+    bottom: 8,
+    right: 6,
+  },
+  name: {
+    fontSize: 24,
+    color: colors.text.primary,
+    fontFamily: fonts.primary[600],
+    textAlign: 'center',
+  },
+  profession: {
+    fontSize: 18,
+    fontFamily: fonts.primary.normal,
+    textAlign: 'center',
+    color: colors.text.secondary,
+    marginTop: 4,
+  },
+});
