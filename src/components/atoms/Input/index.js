0897f5e4456f0bf6efeb6d19d6827b2bbdeb884e
adityacaturputra <adityacaturputra25@gmail.com>
2021-09-27 13:43:45 +0700

    onFocus and onBlur at input form style

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {colors, fonts} from '../../../utils';
 
 const Input = ({label}) => {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+
+  const onBlurForm = () => {
+    setBorder(colors.border);
+  };
+
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={styles.input(border)}
+      />
     </View>
   );
 };
@@ -14,12 +28,12 @@ const Input = ({label}) => {
 export default Input;
 
 const styles = StyleSheet.create({
-  input: {
+  input: border => ({
     padding: 12,
     borderWidth: 1,
-    borderColor: colors.border,
+    borderColor: border,
     borderRadius: 10,
-  },
+  }),
   label: {
     fontSize: 16,
     color: colors.text.secondary,
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Header, Input, Button, Gap} from '../../components';
 import {colors} from '../../utils';
 
@@ -8,18 +8,20 @@ const Register = ({navigation}) => {
     <View style={styles.page}>
       <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
       <View style={styles.content}>
-        <Input label="Full Name" />
-        <Gap height={24} />
-        <Input label="Pekerjaan" />
-        <Gap height={24} />
-        <Input label="Email" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadPhoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input label="Full Name" />
+          <Gap height={24} />
+          <Input label="Pekerjaan" />
+          <Gap height={24} />
+          <Input label="Email" />
+          <Gap height={24} />
+          <Input label="Password" />
+          <Gap height={40} />
+          <Button
+            title="Continue"
+            onPress={() => navigation.navigate('UploadPhoto')}
+          />
+        </ScrollView>
       </View>
     </View>
   );
