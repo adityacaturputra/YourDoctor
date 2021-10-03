8fe81b84ecdd85eebbeaa00929eec690d2b4dff8
adityacaturputra <adityacaturputra25@gmail.com>
2021-10-03 07:27:03 +0700

    add Loading animation

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import {useState} from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
-import {Header, Input, Button, Gap} from '../../components';
+import {Header, Input, Button, Gap, Loading} from '../../components';
 import {Fire} from '../../config';
 import {colors, useForm} from '../../utils';
 
@@ -11,55 +12,62 @@ const Register = ({navigation}) => {
     email: '',
     password: '',
   });
+  const [loading, setLoading] = useState(false);
 
   const onContinue = () => {
     console.log(Fire);
+    setLoading(true);
 
     Fire.auth()
       .createUserWithEmailAndPassword(form.email, form.password)
       .then(success => {
+        setLoading(false);
         console.log('register success: ', success);
       })
       .catch(error => {
+        setLoading(false);
         const errorMessage = error.message;
         console.log('error register: ', errorMessage);
       });
   };
 
   return (
-    <View style={styles.page}>
-      <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
-      <View style={styles.content}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <Input
-            label="Full Name"
-            value={form.fullName}
-            onChangeText={value => setForm('fullName', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Pekerjaan"
-            value={form.profession}
-            onChangeText={value => setForm('profession', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Email"
-            value={form.email}
-            onChangeText={value => setForm('email', value)}
-          />
-          <Gap height={24} />
-          <Input
-            label="Password"
-            value={form.password}
-            onChangeText={value => setForm('password', value)}
-            secureTextEntry
-          />
-          <Gap height={40} />
-          <Button title="Continue" onPress={onContinue} />
-        </ScrollView>
+    <>
+      <View style={styles.page}>
+        <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
+        <View style={styles.content}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Input
+              label="Full Name"
+              value={form.fullName}
+              onChangeText={value => setForm('fullName', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Pekerjaan"
+              value={form.profession}
+              onChangeText={value => setForm('profession', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Email"
+              value={form.email}
+              onChangeText={value => setForm('email', value)}
+            />
+            <Gap height={24} />
+            <Input
+              label="Password"
+              value={form.password}
+              onChangeText={value => setForm('password', value)}
+              secureTextEntry
+            />
+            <Gap height={40} />
+            <Button title="Continue" onPress={onContinue} />
+          </ScrollView>
+        </View>
       </View>
-    </View>
+      {loading && <Loading />}
+    </>
   );
 };
 
--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -8,6 +8,8 @@ const mainColors = {
   gray2: '#E9E9E9',
   gray3: '#EDEEF0',
   blue1: '#0066CB',
+  black1: '#000000',
+  black2: 'rgba(0, 0, 0, 0.5)',
 };
 
 export const colors = {
@@ -36,4 +38,5 @@ export const colors = {
   },
   border: mainColors.gray2,
   cardLight: mainColors.green2,
+  loadingBackground: mainColors.black2,
 };
