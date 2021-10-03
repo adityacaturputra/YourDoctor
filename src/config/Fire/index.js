644794967bcf63653cbdacbd6ec55931206de340
adityacaturputra <adityacaturputra25@gmail.com>
2021-10-03 19:19:52 +0700

    save user data to firebase

--- src/config/Fire/index.js
+++ src/config/Fire/index.js
@@ -4,6 +4,8 @@ firebase.initializeApp({
   apiKey: 'AIzaSyA6s00vpBtlkSpfX5a4GswPpDWrj3j5khc',
   authDomain: 'your-doctor-59164.firebaseapp.com',
   projectId: 'your-doctor-59164',
+  databaseURL:
+    'your-doctor-59164-default-rtdb.asia-southeast1.firebasedatabase.app',
   storageBucket: 'your-doctor-59164.appspot.com',
   messagingSenderId: '46083939976',
   appId: '1:46083939976:web:2c13ea8d362296fafc493d',
