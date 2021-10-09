5972c878abcd83d3d5e225495f02d22e5357d9fc
adityacaturputra <adityacaturputra25@gmail.com>
2021-10-09 14:26:02 +0700

    Login Implementation with firebase

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,29 +1,76 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Input, Link, Button, Gap} from '../../components';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Input, Link, Button, Gap, Loading} from '../../components';
 import {ILLogo} from '../../assets';
-import {colors, fonts} from '../../utils';
+import {colors, fonts, storeData, useForm} from '../../utils';
+import {Fire} from '../../config';
+import {showMessage} from 'react-native-flash-message';
+import {useState} from 'react';
 
 const Login = ({navigation}) => {
+  const [form, setForm] = useForm({email: '', password: ''});
+  const [loading, setLoading] = useState(false);
+
+  const login = () => {
+    setLoading(true);
+    Fire.auth()
+      .signInWithEmailAndPassword(form.email, form.password)
+      .then(res => {
+        setLoading(false);
+        Fire.database()
+          .ref(`users/${res.user.uid}/`)
+          .once('value')
+          .then(res => {
+            if (res.val()) {
+              storeData('user', res.val());
+              navigation.replace('MainApp');
+            }
+          });
+      })
+      .catch(error => {
+        console.log('error: ', error);
+        setLoading(false);
+        showMessage({
+          message: error.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
   return (
-    <View style={styles.page}>
-      <ILLogo />
-      <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
-      <Input label="Email Address" />
-      <Gap height={24} />
-      <Input label="Password" />
-      <Gap height={10} />
-      <Link title="Forgot My Password" fontSize={12} />
-      <Gap height={40} />
-      <Button title="Sign In" onPress={() => navigation.replace('MainApp')} />
-      <Gap height={30} />
-      <Link
-        title="Create New Account"
-        textAlign="center"
-        fontSize={16}
-        onPress={() => navigation.navigate('Register')}
-      />
-    </View>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.page}>
+          <ILLogo />
+          <Text style={styles.title}>Masuk dan mulai berkonsultasi</Text>
+          <Input
+            label="Email Address"
+            value={form.email}
+            onChangeText={value => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={form.password}
+            onChangeText={value => setForm('password', value)}
+            secureTextEntry
+          />
+          <Gap height={10} />
+          <Link title="Forgot My Password" fontSize={12} />
+          <Gap height={40} />
+          <Button title="Sign In" onPress={login} />
+          <Gap height={30} />
+          <Link
+            title="Create New Account"
+            textAlign="center"
+            fontSize={16}
+            onPress={() => navigation.navigate('Register')}
+          />
+        </View>
+      </ScrollView>
+      {loading && <Loading />}
+    </>
   );
 };
 
